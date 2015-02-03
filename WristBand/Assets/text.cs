c7d2307aa3f00ea5cc2b4703811605489c628b47
sek19 <sek19howell@gmail.com>
2015-02-03 13:43:02 -0500

    WristBand
    This is the main functioning device to help people replace their phones

---
+++ WristBand/Assets/text.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class text : MonoBehaviour {
+
+	public string stringToEdit = "Hello World";
+	void OnGUI() {
+		stringToEdit = GUI.TextField(new Rect(10, 10, 200, 20), stringToEdit, 25);
+
+}
+}
